55b24a4c1b263f2820dc0d26674d3ff286ffbd82
Omar Khasawneh <96789418+OMHK@users.noreply.github.com>
2022-03-03 00:21:54 +0300

    added regex and email val

--- Password Generator.py
+++ Password Generator.py
@@ -4,6 +4,7 @@ you can specify the length of the password
 '''
 
 import random as r
+import re
 
 poollist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
             't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
@@ -15,7 +16,8 @@ d = 0
 leng = 0
 thevers = []
 z = True
-
+your_email = ''
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,3})+')
 def generator(lang):
     global password
     global thevers
@@ -27,6 +29,19 @@ def generator(lang):
     for b in range(lang):
         password = password + poollist[thevers[b]]
 
+def isvalid(email):
+    global regex
+    global your_email
+    while True:
+        if re.fullmatch(regex,email):
+            print('your email is:')
+            print(email)
+            break
+        print('Invalid Email Please renter')
+        email = input()
+print('Please enter your email')
+your_email = input()
+isvalid(your_email)
 
 while z:
     print('Please chose a length:')
@@ -35,9 +50,9 @@ while z:
         leng = int(leng)
         break
     print('Please enter a number for your length.')
+
 print('Your new password is')
 generator(leng)
 print(password)
 print('please memorise your new password')
 print('Thank you for trying this code')
-z = False
