7ece7fffd45c1d977d35823e2176c4c870e7c7cd
Omar Khasawneh <omhk97@gmail.com>
2022-03-07 16:54:26 +0300

    add bulletin to copied data

---
+++ Bullet_Time.py
@@ -0,0 +1,13 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip as pyc
+import re
+text = pyc.paste()
+# Separate lines and add stars.
+lines = re.split("nnn",text)
+for i in range(len(lines)): # loop through all indexes for "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+pyc.copy(text)
